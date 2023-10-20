4cf616102de7d190dd8741779424f958332faec5
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-10-20 03:42:50 +0000

    clean up

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 # HF-TP-inference
 PyTorch Native Tensor Parallel for HuggingFace models inference
 
+
+### Run the inference TP(lized) model +Comple
+
+Example of HF llama 7B
+
+```bash
+torchrun --nnodes 1 --nproc_per_node 2 llama-simple.py --model_name meta-llama/Llama-2-7b-chat-hf --compile
+
+```
 ### Convert HF checkpoints to DTensor Checkpoints
 
-Example of llama 7B
+
 
 ```bash
 torchrun --nnodes 1 --nproc_per_node 2 hf_convertor.py --model_name meta-llama/Llama-2-7b-chat-hf --save_checkpoint_dir hf-dtensor-checkpoints
 ```
 
-### Run the inference with TP+compile
+### Run the inference with deferred init TP(lized) model +compile
 
 
 ```bash
-torchrun --nnodes 1 --nproc_per_node 2 llama-simple.py --model_name meta-llama/Llama-2-7b-chat-hf --checkpoint_dir hf-dtensor-checkpoints
+torchrun --nnodes 1 --nproc_per_node 2 llama-simple.py --model_name meta-llama/Llama-2-7b-chat-hf --checkpoint_dir hf-dtensor-checkpoints --compile --meta_device
 
 ```
--- llama-simple.py
+++ llama-simple.py
@@ -20,48 +20,11 @@ from utils import print_submodules, parallelize_llama_MLP_block, tp_llama, _load
 
 # torchrun --nnodes 1 --nproc_per_node 2 llama-simple.py 
 
-# def print_submodules(model):
-#         for name, module in model.named_modules():
-#             print(f"Module name: {name}")
-#             # print(module)
-#             print()
-            
-# def parallelize_llama_MLP_block(model, module_path, mesh):
-#     block = model.get_submodule(module_path)
-#     parallelized_block = parallelize_module(
-#         module=block,
-#         device_mesh=mesh,
-#         parallelize_plan={
-#             "gate_proj": ColwiseParallel(),
-#             "up_proj": ColwiseParallel(),
-#             "down_proj": RowwiseParallel(),
-#         },
-#         # tp_mesh_dim=0,
-#     )
-#     return parallelized_block
 
-# def tp_llama(model, mesh):
-#     for i in range(model.config.num_hidden_layers):
-#         block = parallelize_llama_MLP_block(model, f"model.layers.{i}.mlp", mesh)
-
-# def _load_tp_checkpoints(tp_model,CHECKPOINT_DIR):
-    
-#     local_rank = int(os.environ.get("LOCAL_RANK", -1))
-#     if local_rank == -1:
-#         raise RuntimeError("Expected local_rank to be set, but it is not!")
-#     tp_state_dict = tp_model.state_dict()
-#     dist_cp.load_state_dict(
-#             state_dict=tp_state_dict,
-#             storage_reader=dist_cp.FileSystemReader(CHECKPOINT_DIR),
-#         )
-#     tp_model.load_state_dict(tp_state_dict)
-    
-
-    
-
-# print_submodules(model)
-
-def main (model_name: str= "meta-llama/Llama-2-7b-chat-hf", checkpoint_dir: str ="hf-dtensor-checkpoints"):
+def main (model_name: str = "meta-llama/Llama-2-7b-chat-hf", 
+          checkpoint_dir: str = None, 
+          compile: bool = False,
+          meta_device: bool = False):
     backend = "nccl" 
     dist.init_process_group("nccl")
     _rank = int(os.environ["RANK"])
@@ -73,9 +36,9 @@ def main (model_name: str= "meta-llama/Llama-2-7b-chat-hf", checkpoint_dir: str 
     torch.cuda.set_device(device)
             
     model_name = "meta-llama/Llama-2-7b-chat-hf"
-    with torch.device("meta"):
-        model = LlamaForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
-    # model.to(device)
+    # with torch.device("meta"):
+    model = LlamaForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
+    model.to(device)
     tokenizer = LlamaTokenizer.from_pretrained(model_name)
     # model = BetterTransformer.transform(model)
     mesh = (
@@ -85,23 +48,32 @@ def main (model_name: str= "meta-llama/Llama-2-7b-chat-hf", checkpoint_dir: str 
             ))
             
     tp_llama(model, mesh)
-    model.to_empty(device='cuda')
-    _load_tp_checkpoints(model,"./hf-dtensor-checkpoints")   
+    if meta_device:
+        model.to_empty(device='cuda')
+        _load_tp_checkpoints(model,checkpoint_dir)   
     dummy_input = "what is the recipe of ketchup?"
     dummy_input = tokenizer(dummy_input, return_tensors="pt").to(device)
-
-    with CompileProfiler() as prof:
-        compiled_model = torch.compile(model, backend="inductor")
-        output = compiled_model.generate(
-                    **dummy_input,
-                    max_new_tokens=20,
-                    do_sample=True,
-                    top_p=0.9,
-                    temperature=0.6)
+    if compile:
+        with CompileProfiler() as prof:
+            compiled_model = torch.compile(model, backend="inductor")
+            output = compiled_model.generate(
+                        **dummy_input,
+                        max_new_tokens=20,
+                        do_sample=True,
+                        top_p=0.9,
+                        temperature=0.6)
+            print(tokenizer.decode(output[0], skip_special_tokens=True))
+            print("====================================================")
+            print(prof.report())
+    else:
+        output = model.generate(
+                **dummy_input,
+                max_new_tokens=20,
+                do_sample=True,
+                top_p=0.9,
+                temperature=0.6)
         print(tokenizer.decode(output[0], skip_special_tokens=True))
         print("====================================================")
-        print(prof.report())
-        
         
 if __name__=="__main__":
     fire.Fire(main)
